f4d977d972f838a38eb3ab5d70059ce02d534f5b
Sohan S PUTHRAN <spsohan2106@gmail.com>
2017-08-13 18:40:02 +0530

    Update and rename DiagonalDifference to diagonalDifference.java

--- diagonalDifference.java
+++ diagonalDifference.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class Solution {
+public class DiagonalDifference {
 
     public static void main(String[] args) {
         try(Scanner in = new Scanner(System.in)){
